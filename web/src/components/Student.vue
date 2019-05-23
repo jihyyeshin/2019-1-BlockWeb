c3ace6fa3f7a23760838fe4d15ae3b171261aa8a
jihyyeshin <tlsjh082@gmail.com>
2019-05-23 11:27:48 +0900

    190523 edit student information format

--- web/src/components/Student.vue
+++ web/src/components/Student.vue
@@ -1,45 +1,60 @@
 <template>
-  <div>
-    <b-table striped hover :items="items" :fields="fields"></b-table>
+  <div class="hello">
+    <br/><br/><h1>학생 정보</h1>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+     <b-form-group id="input-group-1" label="학적:" label-for="input-1">
+       <b-form-input
+         id="input-1"
+         v-model="form.id"
+         required
+         placeholder="학적"
+       ></b-form-input>
+     </b-form-group>
+     <b-form-group id="input-group-2" label="전공:" label-for="input-2">
+       <b-form-input
+         id="input-2"
+         v-model="form.passwd"
+         required
+         placeholder="전공"
+       ></b-form-input>
+     </b-form-group>
 
+     <b-button type="submit" variant="primary">Submit</b-button>
+     <b-button type="reset" variant="danger">Reset</b-button>
+   </b-form>
   </div>
+
 </template>
 
 <script>
-import data from '@/data'
-let items = data.Content.sort((a,b) => {return b.content_id - a.content_id})
-items = items.map(contentItem => {return {...contentItem, user_name: data.User.filter(userItem => userItem.user_id === contentItem.user_id)[0].name}})
-
-  export default {
-    data() {
-      return {
-        fields: [
-        {
-          key: 'content_id',
-          label: '글번호',
-        },
-        {
-          key: 'title',
-          label: '제목',
-        },
-        {
-          key: 'created_at',
-          label: '등록일'
-        },
-        {
-          key: 'user_name',
-          label: '글쓴이'
-        },
-      ],
-      items: items
-      }
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      form:{
+      id: '',
+      passwd: '',
     },
-    // methods:{
-    //   rowClick(item, index, e){
-    //     this.$router.push({
-    //       path:'/board/free/detail/${item.content}'
-    //     })
-    //   }
-    // }
+    show: true
   }
+  }
+}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
